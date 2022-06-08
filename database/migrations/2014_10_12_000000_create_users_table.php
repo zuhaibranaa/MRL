d47192d13cb4f1053b6df174575f24dd2de555a3
zuhaib <zuhaibranaa@gmail.com>
2022-06-08 11:20:53 +0500

    resetted

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('gender');
+            $table->string('image');
+            $table->dateTime('DOB');
+            $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
